133271de809594722b8f223af8df237d336f5c38
Nikolay Skryabin <nickskryabin@gmail.com>
2022-06-13 13:11:45 -0700

    First commit

---
+++ src/main/java/com/nikolays/springboot/springbootrestcrudapp/SpringbootRestCrudAppApplication.java
@@ -0,0 +1,13 @@
+package com.nikolays.springboot.springbootrestcrudapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRestCrudAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRestCrudAppApplication.class, args);
+	}
+
+}
