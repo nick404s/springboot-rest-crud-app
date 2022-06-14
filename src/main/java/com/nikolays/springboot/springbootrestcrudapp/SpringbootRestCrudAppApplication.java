159f3ac2305aed129c4f2adeda313b1dfda37ce6
Nikolay Skryabin <nickskryabin@gmail.com>
2022-06-14 11:00:04 -0700

    Add main

--- src/main/java/com/nikolays/springboot/springbootrestcrudapp/SpringbootRestCrudAppApplication.java
+++ src/main/java/com/nikolays/springboot/springbootrestcrudapp/SpringbootRestCrudAppApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringbootRestCrudAppApplication {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(SpringbootRestCrudAppApplication.class, args);
 	}
-
 }
