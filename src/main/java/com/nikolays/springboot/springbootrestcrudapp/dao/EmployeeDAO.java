6d4777d15447a8f8beb682d71041ffc62ac95105
Nikolay Skryabin <nickskryabin@gmail.com>
2022-06-15 12:06:03 -0700

    Add more CRUD methods to the DAO and implement them

--- src/main/java/com/nikolays/springboot/springbootrestcrudapp/dao/EmployeeDAO.java
+++ src/main/java/com/nikolays/springboot/springbootrestcrudapp/dao/EmployeeDAO.java
@@ -7,4 +7,10 @@ import com.nikolays.springboot.springbootrestcrudapp.entity.Employee;
 public interface EmployeeDAO {
 	
 	public List<Employee> findAll();
+	
+	public Employee findById(int anId);
+	
+	public void update(Employee anEmployee);
+	
+	public void deleteById(int anId);
 }
--- src/main/java/com/nikolays/springboot/springbootrestcrudapp/dao/EmployeeHiberImpl.java
+++ src/main/java/com/nikolays/springboot/springbootrestcrudapp/dao/EmployeeHiberImpl.java
@@ -29,18 +29,65 @@ public class EmployeeHiberImpl implements EmployeeDAO {
 	 * Returns all employees from the Employee table.
 	 */
 	@Override
-	@Transactional
 	public List<Employee> findAll() {
 		
 		// get the current session from hibernate
 		Session currentSession = entityManager.unwrap(Session.class);
 		
 		// query to database
-		Query<Employee> aQuery = currentSession.createQuery("FROM Employee", Employee.class);
+		Query<Employee> aQuery = currentSession.createQuery("from Employee", Employee.class);
 		
 		// get all employees
 		List<Employee> employees = aQuery.getResultList();
 		
 		return employees;
+	}
+	
+	
+	/**
+	 * Returns an employee by id from the Employee table.
+	 */
+	@Override
+	public Employee findById(int anId) {
+		
+		// get the current session from hibernate
+		Session currentSession = entityManager.unwrap(Session.class);
+		
+		// get an Employee from database
+		Employee anEmployee = currentSession.get(Employee.class, anId);
+		
+		return anEmployee;
+	}
+
+	/**
+	 * Saves or updates an employee to the Employee table.
+	 */
+	@Override
+	public void update(Employee anEmployee) {
+		
+		// get the current session from hibernate
+		Session currentSession = entityManager.unwrap(Session.class);
+		
+		// save or update an Employee in the database
+		currentSession.saveOrUpdate(anEmployee);
+		
+	}
+
+	/**Deletes an employee from the Employee table.
+	 */
+	@Override
+	public void deleteById(int anId) {
+		
+		// get the current session from hibernate
+		Session currentSession = entityManager.unwrap(Session.class);
+		 
+		// query to database
+		Query aQuery = currentSession.createQuery("delete from Employee where id=:employeeId");
+		
+		// set id parameter
+		aQuery.setParameter("employeeId", anId);
+		
+		// delete employee
+		aQuery.executeUpdate();
 	}
 }
